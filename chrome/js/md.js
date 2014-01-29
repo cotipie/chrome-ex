1a6dd7b1e4da56853818ac9c2c3781180e6ffff3
Tsukasa Nagata <tsukasa.nagata@gmail.com>
2014-01-30 03:35:49 +0900

    initial commit

---
+++ chrome/js/md.js
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////
+// (c)2013 unimal Co.,Ltd. all right reserved.
+// サンプル
+// author nt-uni 2014/01/29
+///////////////////////////////////////////////////////////
+ 
+
+$('.userContent').dblclick(function(){
+	$(this).find('.text_exposed_hide').remove();
+	var md = $(this).find('.text_exposed_root').html();
+	md = md.replace(/<span class=\"text_exposed_show\">/g,'');
+	md = md.replace(/<\/span>/g,'');
+	md = md.replace(/<br>/g,'\n');
+	//md = md.replace(/\"/g,'');
+	$(this).html(marked(md));
+});
